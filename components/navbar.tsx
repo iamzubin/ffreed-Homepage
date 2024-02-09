a82855ec3d2dc92d8374f72d712779e0836eeb76
zubin <zchoudhary.10@gmail.com>
2024-02-09 22:28:54 +0530

    fix: error

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -61,20 +61,6 @@ export const Navbar = () => {
 					</NextLink>
 				</NavbarBrand>
 				<ul className="hidden lg:flex gap-4 justify-start ml-2">
-					{siteConfig.navItems.map((item) => (
-						<NavbarItem key={item.href}>
-							<NextLink
-								className={clsx(
-									linkStyles({ color: "foreground" }),
-									"data-[active=true]:text-primary data-[active=true]:font-medium"
-								)}
-								color="foreground"
-								href={item.href}
-							>
-								{item.label}
-							</NextLink>
-						</NavbarItem>
-					))}
 				</ul>
 			</NavbarContent>
 
