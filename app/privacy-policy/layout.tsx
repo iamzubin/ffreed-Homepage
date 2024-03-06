71587f0c77e9f743384bf7facecedbab7a46ef57
zubin <zchoudhary.10@gmail.com>
2024-03-07 03:18:29 +0530

    changes

---
+++ app/privacy-policy/layout.tsx
@@ -0,0 +1,13 @@
+export default function DocsLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<section className="">
+			<div className="">
+				{children}
+			</div>
+		</section>
+	);
+}
